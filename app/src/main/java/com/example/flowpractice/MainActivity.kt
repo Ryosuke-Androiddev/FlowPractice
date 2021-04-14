4104bf321c2fda5292e165cb98724833e17cf248
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-04-14 23:17:54 +0900

    Introduction of Coroutine Flow!

---
+++ app/src/main/java/com/example/flowpractice/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.flowpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flow = flow {
+            for (i in 1..10){
+                emit(i)
+                delay(1000L)
+            }
+        }
+
+        /**
+         if you do like below , you have to wait 3s because this coroutine runs same thread!!
+
+        GlobalScope.launch {
+        flow.collect {
+        println(it)
+
+        // Just Imagine you buffer this consumer actions!!
+        delay(2000L)
+        }
+        }
+         this is the solution to separate coroutine thread!!*/
+
+        // If you delay 2s you just write buffer after this implementation, coroutine runs different thread!!
+        GlobalScope.launch {
+            flow.buffer().filter{
+                it % 2 == 0
+            }.map{
+                it * it
+            }.collect{
+                println(it)
+                delay(2000L)
+            }
+        }
+    }
+}
